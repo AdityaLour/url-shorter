dc314bbb8a90b581768c279cfe50a5272ae28fb5
Aditya Lour <louraditya6584@gmail.com>
2026-05-06 13:24:11 +0530

    Updated the base62 file

--- util/base62.js
+++ util/base62.js
@@ -1,12 +1,12 @@
 const BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const conversionbase62 = (num) => {
-  let shortcode = "";
+  let shortCode = "";
 
   while (num > 0) {
     const remainder = num % 62;
 
-    shortCode = shortcode + BASE62[remainder];
+    shortCode =  BASE62[remainder] + shortCode ;
     num = Math.floor(num / 62);
   }
 
